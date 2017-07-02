406f10cd26d249776fcd02f91793ebe734b42b0f
tomomo-s <tomomo_0326@yahoo.co.jp>
2017-07-02 06:21:59 -0700

    Add files via upload

---
+++ TranslatorTextDemo/TranslatorTextDemo/AzureAuthToken.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Net;
+/// Portable HttpClient for .NET Framework and Windows Phone
+/// https://blogs.msdn.microsoft.com/bclteam/2013/02/18/portable-httpclient-for-net-framework-and-windows-phone/
+/// System.Net.Httpは、デフォルト使えないので、リンクする必要あり
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TranslatorTextDemo
+{
+    /// <summary>
+    /// Client to call Cognitive Services Azure Auth Token service in order to get an access token.
+    /// Exposes asynchronous as well as synchronous methods.
+    /// </summary>
+    public class AzureAuthToken
+    {
+        /// AzureトークンサービスのURL
+        private static readonly Uri ServiceUrl = new Uri("https://api.cognitive.microsoft.com/sts/v1.0/issueToken");
+
+        ///  Azureサブスクリプションキーヘッダー
+        private const string OcpApimSubscriptionKeyHeader = "Ocp-Apim-Subscription-Key";
+
+        /// Azureトークンサービスキーの有効時間 - 最大値１０分 -
+        private static readonly TimeSpan TokenCacheDuration = new TimeSpan(0, 10, 0);
+
+        /// Azureトークンサービスから取得した最後の有効なトークンの値
+        private string _storedTokenValue = string.Empty;
+
+        /// Azureトークンサービスから取得した最後のトークン時間
+        private DateTime _storedTokenTime = DateTime.MinValue;
+
+        ///  Azureサブスクリプションキー
+        public string SubscriptionKey { get; }
+
+        /// HTTPステータスコード取得(最新トークンサービス)
+        public HttpStatusCode RequestStatusCode { get; private set; }
+
+        /// <summary>
+        /// クライアントアクセストークン作成
+        /// </summary>
+        /// <param name="key">サブスクリプション認証キー</param>
+        public AzureAuthToken(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key), "サブスクリプションキーを指定してください。");
+            }
+
+            this.SubscriptionKey = key;
+            this.RequestStatusCode = HttpStatusCode.InternalServerError;
+        }
+
+        /// <summary>
+        /// Gets a token for the specified subscription.
+        /// </summary>
+        /// <returns>The encoded JWT token prefixed with the string "Bearer ".</returns>
+        /// <remarks>
+        /// This method uses a cache to limit the number of request to the token service.
+        /// A fresh token can be re-used during its lifetime of 10 minutes. After a successful
+        /// request to the token service, this method caches the access token. Subsequent 
+        /// invocations of the method return the cached token for the next 5 minutes. After
+        /// 5 minutes, a new token is fetched from the token service and the cache is updated.
+        /// </remarks>
+        public async Task<string> GetAccessTokenAsync()
+        {
+            if (string.IsNullOrWhiteSpace(this.SubscriptionKey))
+            {
+                return string.Empty;
+            }
+
+            // Re-use the cached token if there is one.
+            if ((DateTime.Now - _storedTokenTime) < TokenCacheDuration)
+            {
+                return _storedTokenValue;
+            }
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = ServiceUrl;
+                request.Content = new StringContent(string.Empty);
+                request.Headers.TryAddWithoutValidation(OcpApimSubscriptionKeyHeader, this.SubscriptionKey);
+                client.Timeout = TimeSpan.FromSeconds(2);
+                var response = await client.SendAsync(request);
+                this.RequestStatusCode = response.StatusCode;
+                response.EnsureSuccessStatusCode();
+                var token = await response.Content.ReadAsStringAsync();
+                _storedTokenTime = DateTime.Now;
+                _storedTokenValue = "Bearer " + token;
+                return _storedTokenValue;
+            }
+        }
+
+        /// <summary>
+        /// Gets a token for the specified subscription. Synchronous version.
+        /// Use of async version preferred
+        /// </summary>
+        /// <returns>The encoded JWT token prefixed with the string "Bearer ".</returns>
+        /// <remarks>
+        /// This method uses a cache to limit the number of request to the token service.
+        /// A fresh token can be re-used during its lifetime of 10 minutes. After a successful
+        /// request to the token service, this method caches the access token. Subsequent 
+        /// invocations of the method return the cached token for the next 5 minutes. After
+        /// 5 minutes, a new token is fetched from the token service and the cache is updated.
+        /// </remarks>
+        public string GetAccessToken()
+        {
+            // Re-use the cached token if there is one.
+            if ((DateTime.Now - _storedTokenTime) < TokenCacheDuration)
+            {
+                return _storedTokenValue;
+            }
+
+            string accessToken = null;
+            var task = Task.Run(async () =>
+            {
+                accessToken = await this.GetAccessTokenAsync();
+            });
+
+            while (!task.IsCompleted)
+            {
+                // System.Threading.Thread.Yield();
+                task.Wait();
+            }
+            if (task.IsFaulted)
+            {
+                throw task.Exception;
+            }
+            if (task.IsCanceled)
+            {
+                throw new Exception("Timeout obtaining access token.");
+            }
+            return accessToken;
+        }
+
+    }
+}
---
+++ TranslatorTextDemo/TranslatorTextDemo/MainPage.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace TranslatorTextDemo
+{
+	public partial class MainPage : ContentPage
+	{
+        // Translator Text Translation APIのサブスクリプションキー（Azureの認証キー1 or キー2）
+        // ※以下をAzureの認証キーを変更すること
+        private const string TEXT_TRANSLATION_API_SUBSCRIPTION_KEY = "ENTER_YOUR_CLIENT_SECRET";
+        // 認証キーオブジェクト
+        private AzureAuthToken tokenProvider;
+        // 言語リストキャッシュ
+        private string[] friendlyName = {" "};
+        // 言語マッピング
+        private Dictionary<string, string> languageCodesAndTitles = new Dictionary<string, string>();
+        
+		public MainPage()
+		{
+			InitializeComponent();
+            // クライアントアクセストークン作成　
+			tokenProvider = new AzureAuthToken(TEXT_TRANSLATION_API_SUBSCRIPTION_KEY);  // Azureの認証キー1 or 認証キー2を設定
+            // 翻訳する言語一覧取得
+            GetLanguagesForTranslate(); 
+            // 翻訳可能な言語設定
+            GetLanguageNamesMethod(tokenProvider.GetAccessToken(), friendlyName);
+            // リスト要素をPickerに設定
+            enumLanguages();
+		}
+		
+        /// <summary>
+        /// 言語一覧表示
+        /// </summary>
+        private void enumLanguages()
+        {
+            // 言語マッピング数
+            var count = languageCodesAndTitles.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                // Xamarin.Forms Picker コントロール
+                LanguagePicker.Items.Add(languageCodesAndTitles.ElementAt(i).Key);
+            }
+
+            if(count != 0)
+            {
+                LanguagePicker.SelectedItem = "Japanese";
+            }
+
+        }
+
+        /// <summary>
+        /// 翻訳開始
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+         private void translateButton_Click(object sender, EventArgs e)
+        {
+            string languageCode;
+            string txtToTranslate = textToTranslate.Text;
+
+            languageCodesAndTitles.TryGetValue(LanguagePicker.SelectedItem.ToString(), out languageCode);
+
+            // デフォルトは英語設定
+            if (languageCode == null) 
+            {
+                languageCode = "en";
+            }
+            // 未入力は処理を終了
+            if (txtToTranslate == null)
+            {
+                return;
+            }
+            // Translate Method
+            // https://msdn.microsoft.com/en-us/library/ff512421.aspx
+            //
+            string uri = string.Format("http://api.microsofttranslator.com/v2/Http.svc/Translate?text=" + System.Net.WebUtility.UrlEncode(txtToTranslate) + "&to={0}", languageCode);
+
+            WebRequest translationWebRequest = WebRequest.Create(uri);
+            translationWebRequest.Headers["Authorization"] = tokenProvider.GetAccessToken();
+
+            WebResponse response = null;
+            var task = Task.Run(async () =>
+            {
+                response = await translationWebRequest.GetResponseAsync();
+            });
+            while (!task.IsCompleted)
+            {
+                task.Wait();
+            }
+
+            Stream stream = response.GetResponseStream();
+            Encoding encode = Encoding.GetEncoding("utf-8");
+            StreamReader translatedStream = new StreamReader(stream, encode);
+            System.Xml.XmlDocument xTranslation = new System.Xml.XmlDocument();
+            xTranslation.LoadXml(translatedStream.ReadToEnd());
+            translatedTextLabel.Text= "翻訳 -->   " + xTranslation.InnerText;
+        }
+
+        /// <summary>
+        /// 翻訳する言語一覧取得
+        /// </summary>
+        private void GetLanguagesForTranslate()
+        {
+           
+            string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForTranslate";
+            WebRequest WebRequest = WebRequest.Create(uri);
+            WebRequest.Headers["Authorization"] = tokenProvider.GetAccessToken();
+
+            WebResponse response = null;
+
+            try
+            {
+                var task = Task.Run(async () =>
+                {
+                    response = await WebRequest.GetResponseAsync();
+                });
+                while (!task.IsCompleted)
+                {
+                    task.Wait();
+                }
+                using (Stream stream = response.GetResponseStream())
+                {
+                    System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(typeof(List<string>));
+                    List<string> languagesForTranslate = (List<string>)dcs.ReadObject(stream);
+                    friendlyName = languagesForTranslate.ToArray();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Dispose();
+                    response = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 翻訳可能な言語設定
+        /// </summary>
+        /// <param name="authToken"></param>
+        /// <param name="languageCodes"></param>
+        private void GetLanguageNamesMethod(string authToken, string[] languageCodes)
+        {
+            // GetLanguageNames Method
+            // https://msdn.microsoft.com/ja-jp/library/ff512399.aspx
+            //
+            string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguageNames?locale=en";
+
+            //
+            // Webリクエスト作成
+            //
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Headers["Authorization"] = tokenProvider.GetAccessToken();
+            request.ContentType = "text/xml";
+            request.Method = "POST";
+            System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(Type.GetType("System.String[]"));
+
+            var task = Task.Run(async () =>
+            {
+                using (System.IO.Stream stream = await request.GetRequestStreamAsync())
+                {
+                    dcs.WriteObject(stream, languageCodes);
+                }
+            });
+            while (!task.IsCompleted)
+            {
+                task.Wait();
+            }
+
+            WebResponse response = null;
+            try
+            {
+                task = Task.Run(async () =>
+                {
+                    response = await request.GetResponseAsync();
+                });
+                while (!task.IsCompleted)
+                {
+                    task.Wait();
+                }
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    string[] languageNames = (string[])dcs.ReadObject(stream);
+
+                    for (int i = 0; i < languageNames.Length; i++)
+                    {
+                        languageCodesAndTitles.Add(languageNames[i], languageCodes[i]); 
+                    }   
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Dispose();
+                    response = null;
+                }
+            }
+        }
+	}
+}
